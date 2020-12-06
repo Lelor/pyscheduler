b78f1bf73b75c2da9ce484785855d4057c588bd1
Lelor <leonardo.rodrigues@sensedata.com.br>
2020-12-05 21:30:23 -0300

    Tracking elapsed time between execution groups and adding indicator to leftover time

--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,7 @@
 #  |  SCHEDULER               - start_datetime          |
 #  |                          - end_datetime            |
 #  |                          - execution_groups        |
+#  |                          - serialize_tasks         |
 #  |                          - serialize_exc_groups    |
 #  |   *Execution groups*                               |
 #  | ------------------------  ------------------------ |
@@ -24,6 +25,9 @@ class Task:
         self.description = description
         self.due_date = due_date
         self.estimated_time = estimated_time
+    
+    def __repr__(self):
+        return f'Task<{self._id}, {self.description}>'
 
     def run(self):
         # Task execution rules would be applied here
@@ -31,7 +35,8 @@ class Task:
         return '<status: success>'
         
 class ExecutionGroup:
-    def __init__(self, *tasks):
+    def __init__(self, start_datetime, *tasks):
+        self.start_datetime = start_datetime
         self.tasks = list(tasks)
 
     def __repr__(self):
@@ -51,10 +56,16 @@ class ExecutionGroup:
     def estimated_execution_time(self):
         return sum(map(lambda t: t.estimated_time, self.tasks))
 
+    @property
+    def estimated_end_time(self):
+        return self.start_datetime - timedelta(hours=self.estimated_execution_time)
+
+
 class Scheduler:
     def __init__(self, start_datetime, end_datetime):
         self.start_datetime = start_datetime
         self.end_datetime = end_datetime
+        self.time_leftover = self.end_datetime - self.start_datetime
         self._execution_groups = []
         self._tasks = None
 
@@ -62,31 +73,36 @@ class Scheduler:
         self._tasks = list(map(lambda task: Task(**task), obj))
 
     def serialize_execution_groups(self, timeframe=timedelta(hours=8)):
-        remaining = self._tasks.copy()
+        remaining_tasks = self._tasks.copy()
         for idx, task in enumerate(self._tasks):
-            # saves a little bit of processing on the last iteration, not really necessary
-            if len(remaining) == 1:
-                self._execution_groups.append(ExecutionGroup(*remaining))
+            # Saves a little bit of processing on the last iteration, not really necessary
+            if len(remaining_tasks) == 1:
+                self._execution_groups.append(ExecutionGroup(self.end_datetime - self.time_leftover, *remaining_tasks))
                 break
             # Iterating over every task twice to compare the sets
             # TODO: Implement logic to take the timeframe limit in consideration
-            if task._id in [r._id for r in remaining]:
-                group = ExecutionGroup(task)
+            if task._id in [r._id for r in remaining_tasks]:
+                group = ExecutionGroup(self.end_datetime - self.time_leftover, task)
                 for comparable_task in self._tasks[idx+1:]:
                     if group.estimated_execution_time + comparable_task.estimated_time <= 8:
                         group.append_task(comparable_task)
                         # reconstructs the remaining set of tasks, may refactor it later
-                        remaining = [i for i in remaining if not (i._id in map(lambda x: x._id, group.tasks))]
+                        remaining_tasks = [i for i in remaining_tasks if not (i._id in map(lambda x: x._id, group.tasks))]
                 self._execution_groups.append(group)
+                self.time_leftover -= timedelta(hours=group.estimated_execution_time)
 
+    # Serializes the jobs json into usable data for the scheduler
     def load_jobs(self, obj):
         self.serialize_tasks(obj)
         self.serialize_execution_groups()
 
 
 if __name__ == '__main__':
-    sch = Scheduler(1, 2)
+    dt_start = datetime.fromisoformat('2019-11-10 09:00:00')
+    dt_end = datetime.fromisoformat('2019-11-11 12:00:00')
+    sch = Scheduler(dt_start, dt_end)
     with open('test.json', 'r') as f:
         example_obj = load(f)
     sch.load_jobs(example_obj)
     print(sch._execution_groups)
+    print(sch.time_leftover)
